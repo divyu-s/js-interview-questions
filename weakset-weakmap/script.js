2ba089b4995a679c5d4702522b23f2682c7ee237
Divyanshu Shukla <shukladivyanshu745893@gmail.com>
2025-01-08 22:42:55 +0530

    weakset and weakmap

---
+++ weakset-weakmap/script.js
@@ -0,0 +1,38 @@
+// Weak Set
+
+// let weakSet = new WeakSet();
+
+// let obj1 = {
+//   name: "divyasnhu",
+// };
+
+// let obj2 = {
+//   name: "divya",
+// };
+
+// weakSet.add(obj1);
+// weakSet.add(obj1);
+// weakSet.add(obj2);
+
+// weakSet.delete(obj1);
+// console.log(weakSet.has(obj2));
+// console.log(weakSet);
+
+// Weak Map
+
+// let weakMap = new WeakMap();
+
+// let obj1 = {
+//   name: "divyasnhu",
+// };
+
+// let obj2 = {
+//   name: "divya",
+// };
+
+// weakMap.set(obj1, "name");
+// weakMap.set(obj2, "name");
+// weakMap.delete(obj2);
+// console.log(weakMap.has(obj1));
+// console.log(weakMap.get(obj1));
+// console.log(weakMap);
